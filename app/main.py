9da5309175e75e00faea09c27bac1404fcda8c6b
Carlos Padin <padin790@gmail.com>
2026-01-21 16:38:22 -0500

    Deleted DB connection on startup for deployment

--- app/main.py
+++ app/main.py
@@ -1,28 +1,27 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
+from fastapi.middleware.cors import CORSMiddleware
+
 from app.core.config import ORIGINS_URL
 from app.core.scheduler import start_scheduler, shutdown_scheduler
-from app.db.models import Base
-from app.db.session import engine
 from app.routers import  market_overview, market_exchanges, daily_volumes
-from fastapi.middleware.cors import CORSMiddleware
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
     # STARTUP
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
-    
     start_scheduler()
     yield
     # SHUTDOWN
     shutdown_scheduler()
 
 app = FastAPI(lifespan=lifespan)
+
+# Routers
 app.include_router(market_overview.router)
 app.include_router(market_exchanges.router)
 app.include_router(daily_volumes.router)
 
+# CORS
 app.add_middleware(
     CORSMiddleware,
     allow_origins=ORIGINS_URL,
