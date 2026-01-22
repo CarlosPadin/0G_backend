9a6c6e006fc987b59870f2cac6dde6cbb489898e
Carlos Padin <padin790@gmail.com>
2026-01-22 10:46:51 -0500

    test

--- app/services/save_market_volumes.py
+++ app/services/save_market_volumes.py
@@ -1,56 +1,56 @@
-from datetime import datetime, timezone
-from sqlalchemy import select, func, and_
-from sqlalchemy.orm import aliased
-from app.db.session import async_session
-from app.db.models import MarketExchanges, MarketDailyVolume
+# from datetime import datetime, timezone
+# from sqlalchemy import select, func, and_
+# from sqlalchemy.orm import aliased
+# from app.db.session import async_session
+# from app.db.models import MarketExchanges, MarketDailyVolume
 
-async def calculate_daily_market_volume():
-    midnight_utc = datetime.now(timezone.utc).replace(
-        hour=0, minute=0, second=0, microsecond=0
-    )
+# async def calculate_daily_market_volume():
+#     midnight_utc = datetime.now(timezone.utc).replace(
+#         hour=0, minute=0, second=0, microsecond=0
+#     )
 
-    me = aliased(MarketExchanges)
+#     me = aliased(MarketExchanges)
 
-    async with async_session() as session:
-        # Last market snapshot
-        subquery = (
-            select(
-                me.exchange_id,
-                me.base_id,
-                me.quote_id,
-                func.max(me.timestamp).label("max_ts"),
-            )
-            .where(me.timestamp < midnight_utc)
-            .group_by(
-                me.exchange_id,
-                me.base_id,
-                me.quote_id,
-            )
-            .subquery()
-        )
+#     async with async_session() as session:
+#         # Last market snapshot
+#         subquery = (
+#             select(
+#                 me.exchange_id,
+#                 me.base_id,
+#                 me.quote_id,
+#                 func.max(me.timestamp).label("max_ts"),
+#             )
+#             .where(me.timestamp < midnight_utc)
+#             .group_by(
+#                 me.exchange_id,
+#                 me.base_id,
+#                 me.quote_id,
+#             )
+#             .subquery()
+#         )
 
-        # Sum of market volumes
-        result = await session.execute(
-            select(func.sum(MarketExchanges.volume_usd_24h))
-            .join(
-                subquery,
-                and_(
-                    MarketExchanges.exchange_id == subquery.c.exchange_id,
-                    MarketExchanges.base_id == subquery.c.base_id,
-                    MarketExchanges.quote_id == subquery.c.quote_id,
-                    MarketExchanges.timestamp == subquery.c.max_ts,
-                )
-            )
-        )
+#         # Sum of market volumes
+#         result = await session.execute(
+#             select(func.sum(MarketExchanges.volume_usd_24h))
+#             .join(
+#                 subquery,
+#                 and_(
+#                     MarketExchanges.exchange_id == subquery.c.exchange_id,
+#                     MarketExchanges.base_id == subquery.c.base_id,
+#                     MarketExchanges.quote_id == subquery.c.quote_id,
+#                     MarketExchanges.timestamp == subquery.c.max_ts,
+#                 )
+#             )
+#         )
 
-        total_volume = result.scalar() or 0.0
+#         total_volume = result.scalar() or 0.0
 
-        # Save data in the database
-        session.add(
-            MarketDailyVolume(
-                timestamp=midnight_utc,
-                volume_usd_24h=total_volume,
-            )
-        )
+#         # Save data in the database
+#         session.add(
+#             MarketDailyVolume(
+#                 timestamp=midnight_utc,
+#                 volume_usd_24h=total_volume,
+#             )
+#         )
 
-        await session.commit()
+#         await session.commit()
